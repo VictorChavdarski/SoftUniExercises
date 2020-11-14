c16edb4abab5eebd84c9e9c435e6b25c95f62f8d
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2020-11-14 17:50:34 +0200

    Add files via upload

---
+++ C#OOP/Raiding/Models/Druid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding.Models
+{
+    public class Druid : BaseHero
+    {
+        private const int POWER = 80;
+        public Druid(string name)
+            : base(name)
+        {
+            this.Power = POWER;
+        }
+
+        public override string CastAbility()
+        {
+            return $"{this.GetType().Name} - {this.Name} healed for {this.Power}";
+        }
+    }
+}
---
+++ C#OOP/Raiding/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Raiding.Models;
+
+namespace Raiding
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            ICollection<BaseHero> heroes = new List<BaseHero>();
+
+            BaseHero hero;
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                string type = Console.ReadLine();
+                if (type == typeof(Paladin).Name)
+                {
+                    hero = new Paladin(name);
+                    heroes.Add(hero);
+                }
+                else if (type == typeof(Druid).Name)
+                {
+                    hero = new Druid(name);
+                    heroes.Add(hero);
+                }
+                else if (type == typeof(Rogue).Name)
+                {
+                    hero = new Rogue(name);
+                    heroes.Add(hero);
+                }
+                else if (type ==typeof(Warrior).Name)
+                {
+                    hero = new Warrior(name);
+                    heroes.Add(hero);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid hero!");
+                    i--;
+                }
+            }
+
+            int bossPower = int.Parse(Console.ReadLine());
+            int heroesPowerSum =0;
+            foreach (var player in heroes )
+            {
+                Console.WriteLine(player.CastAbility());
+                heroesPowerSum += player.Power;
+            }
+
+            if (heroesPowerSum >= bossPower)
+            {
+                Console.WriteLine("Victory!");
+            }
+            else
+            {
+                Console.WriteLine("Defeat...");
+            }
+        }
+    }
+}
