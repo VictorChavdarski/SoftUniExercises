c2c3cf329a35c4b1d82b1f267886aa50f1c281bd
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2020-11-16 16:49:34 +0200

    Add files via upload

---
+++ C#Advanced/Exam/ProblemOne/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ProblemOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasksInput = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var threadsInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int taskToKill = int.Parse(Console.ReadLine());
+
+            var threadsQueue = new Queue<int>(threadsInput);
+            var tasksStack = new Stack<int>(tasksInput);
+
+            int taskKiller = 0;
+
+            while (threadsQueue.Count > 0 || tasksStack.Count > 0)
+            {
+                int currentTask = tasksStack.Peek();
+                int currentThread = threadsQueue.Peek();
+                if (currentTask == taskToKill)
+                {
+                    taskKiller = currentThread;
+                    break;
+                }
+                if (currentThread >= currentTask)
+                {
+                    tasksStack.Pop();
+                    threadsQueue.Dequeue();
+                }
+                if (currentThread < currentTask)
+                {
+                    threadsQueue.Dequeue();
+                }
+            }
+            Console.WriteLine($"Thread with value {taskKiller} killed task {taskToKill}");
+            Console.WriteLine(string.Join(' ', threadsQueue));
+
+        }
+    }
+}
+
---
+++ C#Advanced/Exam/ProblemThree/Classroom.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        public readonly List<Student> students;
+        private int capacity;
+
+        private Classroom()
+        {
+            this.students = new List<Student>();
+        }
+        public Classroom(int capacity)
+            :this()
+        {
+            this.Capacity = capacity;
+        }
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+            set
+            {
+                capacity = value;
+            }
+        }
+        public int Count => this.students.Count;
+
+        public string RegisterStudent(Student student)
+        {
+            if (this.students.Count + 1 <= this.Capacity)
+            {
+                this.students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student student = this.students.FirstOrDefault(n => n.FirstName == firstName && n.LastName == lastName);
+            if (student != null)
+            {
+                this.students.Remove(student);
+                return $"Dismissed student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                 return "Student not found";
+            }
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            List<Student> list = this.students.Where(x => x.Subject == subject).ToList();
+            StringBuilder sb = new StringBuilder();
+            if (list.Count > 0)
+            {
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine("Students:");
+                foreach (Student student in list)
+                {
+                    sb.AppendLine($"{student.FirstName} {student.LastName}");
+                }
+                return sb.ToString().TrimEnd();
+            }
+            else
+            {
+               sb.AppendLine("No students enrolled for the subject");
+                return sb.ToString().TrimEnd();
+            }
+        }
+        public int GetStudentsCount()
+        {
+            return this.students.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student student = this.students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+            return student;
+        }
+
+    }
+}
