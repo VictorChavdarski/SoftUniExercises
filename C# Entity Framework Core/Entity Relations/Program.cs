18bb1f538482061ecc9c70eea3986d6c4c75b7b3
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-01 14:55:44 +0200

    Add files via upload

---
+++ C# Entity Framework Core/Entity Relations/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P01_StudentSystem
+{
+    using P01_StudentSystem.Data;
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var context = new StudentSystemContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+        }
+    }
+}
