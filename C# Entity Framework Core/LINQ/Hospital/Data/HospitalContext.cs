a795d0194cf59e600bedfc5fb59e460667a3b79f
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-02 00:34:50 +0200

    Add files via upload

---
+++ C# Entity Framework Core/LINQ/Hospital/Data/HospitalContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext()
+        {
+
+        }
+
+        protected HospitalContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+
+        public DbSet<Diagnose> Diagnoses { get; set; }
+
+        public DbSet<Medicament> Medicaments { get; set; }
+
+        public DbSet<Patient> Patients { get; set; }
+
+        public DbSet<PatientMedicament> PatientMedicaments { get; set; }
+
+        public DbSet<Visitation> Visitations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=Hospital;Integrated Security=true");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Diagnose>(x =>
+            
+                x.HasKey(x => x.DagnoseId)
+            );
+
+            modelBuilder.Entity<PatientMedicament>(x =>
+            {
+                x.HasKey(x => new { x.PatientId, x.MedicamentId });
+            });
+
+            modelBuilder.Entity<Visitation>(x =>
+
+               x.HasKey(x => x.VisitationsId)
+           );
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ C# Entity Framework Core/LINQ/Hospital/Data/Models/Patient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Patient
+    {
+        public Patient()
+        {
+            Prescriptions = new HashSet<PatientMedicament>();
+            Diagnoses = new HashSet<Diagnose>();
+            Visitations = new HashSet<Visitation>();
+        }
+
+
+        public int PatientId { get; set; }
+
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [MaxLength(250)]
+        public string Address { get; set; }
+
+        [MaxLength(80)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public bool HasInsurance { get; set; }
+
+        public virtual ICollection<PatientMedicament> Prescriptions { get; set; }
+
+        public ICollection<Diagnose> Diagnoses { get; set; }
+
+        public ICollection<Visitation> Visitations { get; set; }
+
+    }
+}
