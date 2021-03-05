8074fd711e2e7e181787d21ae03767fd35e9ae73
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-05 22:33:31 +0200

    Update Program.cs

--- C# Fundamentals/Exam One/The Imitation Game/Program.cs
+++ C# Fundamentals/Exam One/The Imitation Game/Program.cs
@@ -6,7 +6,44 @@ namespace _01.TheImitationGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputMessage = Console.ReadLine();
+
+            string command = string.Empty;
+
+            while ((command=Console.ReadLine())!="Decode")
+            {
+                string[] input = command.Split("|");
+                string commOne = input[0];
+                if (commOne=="Move")
+                {
+                    int count = int.Parse(input[1]);
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < count; i++)
+                    {
+                        sb.Append(inputMessage[i]);
+                    }
+                    string substring = sb.ToString();
+                    inputMessage= inputMessage.Replace(substring, string.Empty);
+                    StringBuilder newsb = new StringBuilder(inputMessage);
+                    newsb.Append(substring);
+                    inputMessage = newsb.ToString();
+                }
+                else if (commOne=="Insert")
+                {
+                    int index = int.Parse(input[1]);
+                    string value = input[2];
+                   inputMessage = inputMessage.Insert(index, value);
+                }
+                else if (commOne=="ChangeAll")
+                {
+                    string substring = input[1];
+                    string replacement = input[2];
+
+                    inputMessage = inputMessage.Replace(substring, replacement);
+                }
+            }
+
+            Console.WriteLine($"The decrypted message is: {inputMessage}");
         }
     }
 }
