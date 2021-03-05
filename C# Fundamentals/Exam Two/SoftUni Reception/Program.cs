c316dc1ed346057c0e0d18c19475f1cdd6f27859
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-05 22:35:02 +0200

    Update Program.cs

--- C# Fundamentals/Exam Two/SoftUni Reception/Program.cs
+++ C# Fundamentals/Exam Two/SoftUni Reception/Program.cs
@@ -6,7 +6,28 @@ namespace _01.SoftUniReception
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             int employeeOne = int.Parse(Console.ReadLine());
+            int employeeTwo = int.Parse(Console.ReadLine());
+            int employeeThree = int.Parse(Console.ReadLine());
+
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            int studentsPerHour = employeeOne + employeeTwo + employeeThree;
+
+            int hours = 0;
+
+            while (studentsCount > 0)
+            {
+                hours++;
+                if (hours%4==0)
+                {
+                    continue;
+                }
+
+                studentsCount -= studentsPerHour;
+            }
+            Console.WriteLine($"Time needed: {hours}h.");
+
         }
     }
 }
