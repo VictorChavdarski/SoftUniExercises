55d14b721f50bdbf5c046229eda1261385c74b32
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-05 22:35:32 +0200

    Update Program.cs

--- C# Fundamentals/Exam Two/Numbers/Program.cs
+++ C# Fundamentals/Exam Two/Numbers/Program.cs
@@ -6,7 +6,36 @@ namespace _03.Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> sequence = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            double average = Math.Round(sequence.Average(), 2);
+
+            if (sequence.Count == 1 || average == sequence[sequence.IndexOf(sequence[0])])
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                if (sequence[i] <= average)
+                {
+                    sequence.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            sequence.Sort();
+            sequence.Reverse();
+           
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.Write(sequence[i] + " ");
+            }
         }
     }
 }
