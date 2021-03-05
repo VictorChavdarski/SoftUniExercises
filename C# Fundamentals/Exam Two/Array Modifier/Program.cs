af58b95b8d034f709b600dcb47636c16e2f8fbcd
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-05 22:34:27 +0200

    Update Program.cs

--- C# Fundamentals/Exam Two/Array Modifier/Program.cs
+++ C# Fundamentals/Exam Two/Array Modifier/Program.cs
@@ -6,7 +6,47 @@ namespace _02.ArrayModifier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string command = string.Empty;
+
+            while ((command=Console.ReadLine())!="end")
+            {
+                string[] input = command.Split();
+                string commandOne = input[0];
+
+                if (commandOne == "swap")
+                {
+                    int indexOne = int.Parse(input[1]);
+                    int indexTwo = int.Parse(input[2]);
+
+                    int temp = array[indexOne];
+                    array[indexOne] = array[indexTwo];
+                    array[indexTwo] = temp;
+                }
+                else if (commandOne == "multiply")
+                {
+                    int indexOne = int.Parse(input[1]);
+                    int indexTwo = int.Parse(input[2]);
+                    if (indexOne >= 0 && indexOne <array.Length && indexTwo>=0 && indexTwo<array.Length)
+                    {
+                    array[indexOne] = array[indexOne] * array[indexTwo];
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (commandOne=="decrease")
+                {
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        array[i] = array[i] - 1;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ",array));
         }
     }
 }
