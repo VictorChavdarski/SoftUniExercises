ea4fca6afe12260abdaee4e0ff4ebf9acfdd0d61
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-08 13:17:07 +0200

    Add files via upload

---
+++ C# Entity Framework Core/LINQ/MusicHub/StartUp.cs
@@ -0,0 +1,115 @@
+﻿namespace MusicHub
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context = 
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+
+            var result = ExportSongsAboveDuration(context, 4);
+
+            Console.WriteLine(result);
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            var albumInfo = context.Producers
+                .FirstOrDefault(x => x.Id == producerId)
+                .Albums
+                .Select(x =>
+                new
+                {
+                    AlbumName = x.Name,
+                    ReleaseDate = x.ReleaseDate,
+                    ProducerName = x.Producer.Name,
+                    Songs = x.Songs.Select(s => new 
+                    {   
+                        s.Name,
+                        s.Price, 
+                        s.Writer 
+                    })
+                    .OrderByDescending(x => x.Name)
+                    .ThenBy(x=>x.Writer)
+                    .ToList(),
+                    AlbumPrice = x.Price
+                })
+                .OrderByDescending(x => x.AlbumPrice)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var album in albumInfo )
+            {
+                sb.AppendLine($"-AlbumName: {album.AlbumName}")
+                    .AppendLine($"-ReleaseDate: {album.ReleaseDate:MM/dd/yyyy}")
+                    .AppendLine($"-ProducerName: {album.ProducerName}")
+                    .AppendLine("-Songs:");
+
+                int counter = 1;
+
+                foreach (var song in album.Songs)
+                {
+                    sb.AppendLine($"---#{counter++}")
+                        .AppendLine($"---SongName: {song.Name}")
+                        .AppendLine($"---Price: {song.Price:f2}")
+                        .AppendLine($"---Writer: {song.Writer.Name}");
+                }
+
+                sb.AppendLine($"-AlbumPrice: {album.AlbumPrice:f2}");
+            }
+
+            var result = sb.ToString().TrimEnd();
+            return result;
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            var songs = context.Songs
+                .ToList()
+                .Where(x => x.Duration.TotalSeconds > duration)
+                .Select(x =>
+                new
+                {
+                    SongName = x.Name,
+                    Writer = x.Writer.Name,
+                    PerfromerFullName = x.SongPerformers
+                                        .Select(x => x.Performer.FirstName + " " + x.Performer.LastName)
+                                        .FirstOrDefault(),
+                    AlbumProducer = x.Album.Producer.Name,
+                    Duration = x.Duration
+                })
+                .OrderBy(x => x.SongName)
+                .ThenBy(x => x.Writer)
+                .ThenBy(x => x.PerfromerFullName);
+                
+
+            var sb = new StringBuilder();
+
+            int counter = 1;
+
+            foreach (var song in songs)
+            {
+                sb.AppendLine($"-Song #{counter++}")
+                    .AppendLine($"---SongName: {song.SongName}")
+                    .AppendLine($"---Writer: {song.Writer}")
+                    .AppendLine($"---Performer: {song.PerfromerFullName}")
+                    .AppendLine($"---AlbumProducer: {song.AlbumProducer}")
+                    .AppendLine($"---Duration: {song.Duration}");
+            }
+
+            var result = sb.ToString().TrimEnd();
+
+            return result;
+                
+        }
+    }
+}
