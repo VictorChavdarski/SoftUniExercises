32001c7af50bc01fa462170a16845d11d68aa5a2
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-09 22:11:32 +0200

    Add files via upload

---
+++ C#Advanced/Exam Four/Bee/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace _04._02BeeDescription
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] field = new char[n, n];
+
+            int beeRow = -1;
+            int beeCol = -1;
+
+            int flowersCount = 0;
+
+
+            for (int row = 0; row < n; row++)
+            {
+                string line = Console.ReadLine();
+                for (int col = 0; col < line.Length; col++)
+                {
+                    field[row, col] = line[col];
+                    if (field[row, col] == 'B')
+                    {
+                        beeRow = row;
+                        beeCol = col;
+                    }
+                }
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                field[beeRow, beeCol] = '.';
+                if (command == "up")
+                {
+
+                    beeRow--;
+                }
+                else if (command == "down")
+                {
+
+                    beeRow++;
+                }
+                else if (command == "left")
+                {
+
+                    beeCol--;
+                }
+                else if (command == "right")
+                {
+
+                    beeCol++;
+                }
+                if (!IsSafe(field, beeRow, beeCol))
+                {
+                    Console.WriteLine("The bee got lost!");
+                    break;
+                }
+
+                if (field[beeRow, beeCol] == 'O')
+                {
+                    field[beeRow, beeCol] = '.';
+                    if (command == "up")
+                    {
+                        beeRow--;
+                    }
+                    else if (command == "down")
+                    {
+                        beeRow++;
+                    }
+                    else if (command == "left")
+                    {
+                        beeCol--;
+                    }
+                    else if (command == "right")
+                    {
+                        beeCol++;
+                    }
+                    if (field[beeRow, beeCol] == 'f')
+                    {
+                        field[beeRow, beeCol] = '.';
+                        flowersCount++;
+                    }
+                }
+
+                if (field[beeRow, beeCol] == 'f')
+                {
+                    flowersCount++;
+                }
+
+                field[beeRow, beeCol] = 'B';
+            }
+            if (flowersCount < 5)
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - flowersCount} flowers more");
+            }
+            else
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {flowersCount} flowers!");
+            }
+            PrintField(field);
+        }
+
+        static bool IsSafe(char[,] field, int row, int col)
+        {
+            if (row < 0 || col < 0)
+            {
+                return false;
+            }
+            if (row >= field.GetLength(0) || col >= field.GetLength(1))
+            {
+                return false;
+            }
+            return true;
+        }
+        static void PrintField(char[,] field)
+        {
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    Console.Write(field[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#Advanced/Exam Four/Vet Clinic/Clinic.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private readonly List<Pet> data;
+        private Clinic()
+        {
+            this.data = new List<Pet>();
+        }
+        public Clinic(int capacity)
+            :this()
+        {
+            this.Capacity = capacity;
+        }
+        public int Capacity { get; set; }
+
+        public int Count => this.data.Count;
+
+        public void Add(Pet pet)
+        {
+            if (this.data.Count+1<=this.Capacity)
+            {
+                this.data.Add(pet);
+            }
+        }
+        public bool Remove(string name)
+        {
+            Pet pet = this.data.FirstOrDefault(x => x.Name == name);
+            if (pet!=null)
+            {
+                this.data.Remove(pet);
+                return true;
+            }
+            return false;
+        }
+        public Pet GetPet(string name,string owner)
+        {
+            Pet pet = this.data.FirstOrDefault(x => x.Name == name && x.Owner == owner);
+            if (pet!=null)
+            {
+                return pet;
+            }
+            return null;
+        }
+        public Pet GetOldestPet()
+        {
+            Pet pet = this.data.OrderByDescending(x => x.Age).FirstOrDefault();
+            return pet;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+            foreach (Pet pet in this.data)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
