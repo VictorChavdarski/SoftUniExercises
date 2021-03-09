089a7c0ecfc433ec6044051ed95662688d524816
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-09 22:04:06 +0200

    Add files via upload

---
+++ C#Advanced/Exam Three/Bombs/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._01Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bombsEffects = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var bombsCasing = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            var bombEffectQueue = new Queue<int>(bombsEffects);
+            var bombsCasingStack = new Stack<int>(bombsCasing);
+
+            bool isTrue = true;
+            var createdBombsCount = new Dictionary<string, int>()
+            {
+                {"Datura Bombs",0},
+                {"Cherry Bombs",0 },
+                {"Smoke Decoy Bombs",0}
+            };
+
+            var materialsTable = new Dictionary<int, string>()
+            {
+                {40,"Datura bombs"},
+                {60, "Cherry bombs"},
+                {120, "Smoke decoy bombs"}
+            };
+
+            while (bombsCasingStack.Count > 0 && bombEffectQueue.Count > 0)
+            {
+                if (createdBombsCount["Datura Bombs"] >= 3 && createdBombsCount["Cherry Bombs"] >= 3 && createdBombsCount["Smoke Decoy Bombs"] >= 3)
+                {
+                    Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+                    isTrue = false;
+                    break;
+                }
+                var bombEffect = bombEffectQueue.Peek();
+                var bombCasing = bombsCasingStack.Peek();
+                var result = bombEffect + bombCasing;
+
+                if (materialsTable.ContainsKey(result))
+                {
+                    if (result == 40)
+                    {
+                        createdBombsCount["Datura Bombs"]++;
+                    }
+                    else if (result == 60)
+                    {
+                        createdBombsCount["Cherry Bombs"]++;
+                    }
+                    else
+                    {
+                        createdBombsCount["Smoke Decoy Bombs"]++;
+                    }
+                    bombEffectQueue.Dequeue();
+                    bombsCasingStack.Pop();
+                }
+                else
+                {
+                    int caseDecrease = bombCasing - 5;
+                    bombsCasingStack.Pop();
+                    bombsCasingStack.Push(caseDecrease);
+                }
+            }
+            if (isTrue)
+            {
+                Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+            }
+
+
+            if (bombEffectQueue.Count > 0)
+            {
+                Console.WriteLine($"Bomb Effects: { string.Join(", ", bombEffectQueue)}");
+            }
+            else
+            {
+                Console.WriteLine("Bomb Effects: empty");
+            }
+            if (bombsCasingStack.Count > 0)
+            {
+                Console.WriteLine($"Bomb Casings: {string.Join(", ", bombsCasingStack)}");
+            }
+            else
+            {
+                Console.WriteLine("Bomb Casings: empty");
+            }
+
+            foreach (var bomb in createdBombsCount.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{bomb.Key}: {bomb.Value}");
+            }
+
+        }
+    }
+}
---
+++ C#Advanced/Exam Three/Parking/Parking.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parking
+{
+    public class Parking
+    {
+        private readonly List<Car> data;
+
+        public Parking()
+        {
+            this.data = new List<Car>();
+        }
+        public Parking(string type, int capacity)
+            : this()
+        {
+            this.Type = type;
+            this.Capacity = capacity;
+        }
+
+        public int Count => this.data.Count;
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+
+        public void Add(Car car)
+        {
+            if (this.data.Count + 1 <= this.Capacity)
+            {
+                this.data.Add(car);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            Car car = this.data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+
+            if (car != null)
+            {
+                this.data.Remove(car);
+                return true;
+            }
+            return false;
+        }
+
+        public Car GetLatestCar()
+        {
+            Car car = this.data.OrderByDescending(x => x.Year).FirstOrDefault();
+            if (this.data.Count > 0)
+            {
+                return car;
+            }
+            return null;
+        }
+
+        public Car GetCar(string manufacturer, string model)
+        {
+            Car car = this.data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+            if (car != null)
+            {
+                return car;
+            }
+            return null;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The cars are parked in {this.Type}:");
+            foreach (Car car in this.data)
+            {
+                sb.AppendLine(car.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
