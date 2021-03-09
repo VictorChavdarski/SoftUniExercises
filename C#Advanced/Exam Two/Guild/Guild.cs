b4e43c75bb3e42aaeafff41fa61d68ece6a71f29
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-09 22:00:31 +0200

    Add files via upload

---
+++ C#Advanced/Exam Two/Guild/Guild.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private List<Player> data;
+        private Guild()
+        {
+            this.data = new List<Player>();
+        }
+        public Guild(string name,int capacity)
+            :this()
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+        }
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => this.data.Count;
+
+        public void AddPlayer(Player player)
+        {
+            if (this.data.Count + 1 <= this.Capacity)
+            {
+                this.data.Add(player);
+            }
+        }
+        public bool RemovePlayer(string name)
+        {
+            Player player = this.data.FirstOrDefault(n => n.Name == name);
+            if (player!=null)
+            {
+                this.data.Remove(player);
+                return true;
+            }
+            return false;
+        }
+        public void PromotePlayer(string name)
+        {
+            Player player = this.data.FirstOrDefault(n => n.Name == name);
+            if (player.Rank == "Trial")
+            {
+                player.Rank = "Member";
+            }
+        }
+        public void DemotePlayer(string name)
+        {
+            Player player = this.data.FirstOrDefault(n => n.Name == name);
+            if (player.Rank == "Member")
+            {
+                player.Rank = "Trial";
+            }
+        }
+        public Player[] KickPlayersByClass(string classText)
+        {
+            Player[] arr;
+            arr = this.data.Where(x => x.ClassText == classText).ToArray();
+            this.data = this.data.Where(x => x.ClassText != classText).ToList();
+            return arr;
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Players in the guild: {this.Name}");
+            foreach (var player in this.data)
+            {
+                sb.AppendLine(player.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#Advanced/Exam Two/Lootbox/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _07._01LootBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLootBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var secondLootBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var firtsQueue = new Queue<int>(firstLootBox);
+            var secondStack = new Stack<int>(secondLootBox);
+
+            int loot = 0;
+
+            while (firtsQueue.Count > 0 || secondStack.Count > 0)
+            {
+                if (firtsQueue.Count == 0)
+                {
+                    Console.WriteLine("First lootbox is empty");
+                    break;
+                }
+                else if (secondStack.Count == 0)
+                {
+                    Console.WriteLine("Second lootbox is empty");
+                    break;
+                }
+                var sum = firtsQueue.Peek() + secondStack.Peek();
+                if (sum % 2 == 0)
+                {
+                    loot += sum;
+                    firtsQueue.Dequeue();
+                    secondStack.Pop();
+                }
+                else
+                {
+                    var movedItem = secondStack.Peek();
+                    secondStack.Pop();
+                    firtsQueue.Enqueue(movedItem);
+                }
+            }
+
+            if (loot >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {loot}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {loot}");
+            }
+
+
+
+
+        }
+    }
+}
