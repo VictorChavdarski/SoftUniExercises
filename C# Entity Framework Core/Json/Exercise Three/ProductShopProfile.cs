01b217843a47147ff239d02722e58201efda006b
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-15 13:13:36 +0200

    Add files via upload

---
+++ C# Entity Framework Core/Json/Exercise Three/ProductShopProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProductShop.DataTransferObjects;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            this.CreateMap<UserInputModel, User>();
+            this.CreateMap<ProductInputModel, Product>();
+            this.CreateMap<CategoryInputModel, Category>();
+        }
+    }
+}
