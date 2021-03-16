adb30bc9f232a87981f3d50681d386b08f22a491
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-16 17:29:09 +0200

    Add files via upload

---
+++ C# Entity Framework Core/Json/Exercise Nine/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTO;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var json = File.ReadAllText("../../../Datasets/suppliers.json");
+
+            Console.WriteLine(ImportSuppliers(context, json));
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliersDtos = JsonConvert.DeserializeObject<IEnumerable<ImportSupliersInputModels>>(inputJson);
+
+            var suppliers = suppliersDtos.Select(x => new Supplier
+            {
+                Name = x.Name,
+                IsImporter = x.IsImporter
+            })
+                .ToList();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
+
+        }
+    }
+}
