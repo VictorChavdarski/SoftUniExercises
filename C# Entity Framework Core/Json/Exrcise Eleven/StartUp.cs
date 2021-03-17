1a395ec5ac5efe9c6e87ebcf40ee288d15867377
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-17 09:22:11 +0200

    Add files via upload

---
+++ C# Entity Framework Core/Json/Exrcise Eleven/StartUp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTO;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            //var suppliersJson = File.ReadAllText("../../../Datasets/suppliers.json");
+            //var partsJson = File.ReadAllText("../../../Datasets/parts.json");
+            var carsJson = File.ReadAllText("cars.json");
+
+            Console.WriteLine(ImportCars(context, carsJson));
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            var cars = JsonConvert.DeserializeObject<IEnumerable<Car>>(inputJson);
+
+            context.Cars.AddRange(cars);
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Count()}.";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var suppliedIds = context.Suppliers
+                .Select(x => x.Id)
+                .ToArray();
+
+            var parts = JsonConvert
+                .DeserializeObject<IEnumerable<Part>>(inputJson)
+                .Where(s => suppliedIds.Contains(s.SupplierId))
+                .ToList();
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count}.";
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliersDtos = JsonConvert.DeserializeObject<IEnumerable<ImportSupliersInputModels>>(inputJson);
+
+            var suppliers = suppliersDtos.Select(x => new Supplier
+            {
+                Name = x.Name,
+                IsImporter = x.IsImporter
+            })
+                .ToList();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
+
+        }
+    }
+}
