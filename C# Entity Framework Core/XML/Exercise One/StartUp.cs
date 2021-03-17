d72b8a376eb271b60cfd276d7c9cc4f14e36046c
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-17 10:04:44 +0200

    Add files via upload

---
+++ C# Entity Framework Core/XML/Exercise One/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using CarDealer.Data;
+using CarDealer.DataTransferObjects.Input;
+using CarDealer.Models;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var supplierXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+
+            var result = ImportSuppliers(context, supplierXml);
+
+            Console.WriteLine(result);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(SupplierInputModel[]), new XmlRootAttribute("Supplier"));
+            var textRead = new StringReader(inputXml);
+
+            var suppliersDto = xmlSerializer
+                .Deserialize(textRead) as SupplierInputModel[];
+
+            var suppliers = suppliersDto.Select(x => new Supplier
+            {
+                Name = x.Name,
+                IsImporter = x.IsImporter
+            })
+                .ToList();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+              
+            return $"Successfully imported {suppliers.Count}";
+
+
+        }
+    }
+}
