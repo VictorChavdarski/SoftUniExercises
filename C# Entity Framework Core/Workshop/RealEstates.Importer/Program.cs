e5255421b30f77145d5c9c07ebe198247d5d04a1
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-19 10:54:00 +0200

    Add files via upload

---
+++ C# Entity Framework Core/Workshop/RealEstates.Importer/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace RealEstates.Importer
+{
+    using RealEstates.Data;
+    using RealEstates.Services;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text.Json;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ImportJsonFile("imot.bg-houses-Sofia-raw-data-2021-03-18.json");
+            System.Console.WriteLine();
+            ImportJsonFile("imot.bg-raw-data-2021-03-18.json");
+        }
+
+        private static void ImportJsonFile(string fileName)
+        {
+            var dbContext = new ApplicationDbContext();
+            IPropertiesService propertiesService = new PropertiesService(dbContext);
+
+            var properties =
+                JsonSerializer.Deserialize<IEnumerable<PropertyAsJson>>(
+                    File.ReadAllText(fileName));
+
+            foreach (var jsonProp in properties)
+            {
+                propertiesService.Add(jsonProp.District, jsonProp.Floor, jsonProp.TotalFloors,
+                    jsonProp.Size, jsonProp.YardSize, jsonProp.Year,
+                    jsonProp.Type, jsonProp.BuildingType, jsonProp.Price);
+
+                Console.WriteLine(".");
+            }
+        }
+    }
+}
