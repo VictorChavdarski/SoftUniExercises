3c2fbda537aab95f41d7e5d2ea37e67f3ded7c60
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-19 10:53:28 +0200

    Add files via upload

---
+++ C# Entity Framework Core/Workshop/RealEstates.Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿namespace RealEstates.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using RealEstates.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Property> Properties { get; set; }
+
+        public DbSet<BuildingType> BuildingTypes { get; set; }
+
+        public DbSet<District> Districts { get; set; }
+
+        public DbSet<PropertyType> PropertyTypes { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=RealEstates;Integrated Security=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
