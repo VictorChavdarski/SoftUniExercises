27984cd38c9e269118268255a6af7e9505e53ef6
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-23 22:52:46 +0200

    Add files via upload

---
+++ C# Entity Framework Core/ASP.NET Core Workshop/Quiz.Data/ApplicationDbContext.cs
@@ -0,0 +1,43 @@
+﻿namespace Quiz.Data
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using Quiz.Models;
+
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<Answer> Answers { get; set; }
+
+        public DbSet<Models.Quiz> Quizzes { get; set; }
+
+        public DbSet<Question> Questions { get; set; }
+
+        public  DbSet<UserAnswer> UserAnswers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Answer>()
+                .HasOne(x => x.Question)
+                .WithMany(x => x.Answers)
+                .HasForeignKey(x => x.QuestionId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Question>()
+                .HasOne(x => x.Quiz)
+                .WithMany(x => x.Questions)
+                .HasForeignKey(x => x.QuizId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
---
+++ C# Entity Framework Core/ASP.NET Core Workshop/Quiz.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace Quiz.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.Extensions.Configuration;
+    using System.IO;
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var opitonBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            opitonBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new ApplicationDbContext(opitonBuilder.Options);
+        }
+    }
+}
