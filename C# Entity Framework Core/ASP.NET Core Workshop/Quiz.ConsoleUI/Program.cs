1b7cbaecd0557c3f12330e66b718b94f5e1b2471
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-23 22:52:13 +0200

    Add files via upload

---
+++ C# Entity Framework Core/ASP.NET Core Workshop/Quiz.ConsoleUI/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Quiz.ConsoleUI
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Quiz.Data;
+    using Quiz.Services;
+    using System;
+    using System.IO;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString
+            ("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(options =>
+              options.SignIn.RequireConfirmedAccount = true)
+              .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddTransient<IQuizService, QuizService>();
+            services.AddTransient<IQuestionService, QuestionService>();
+            services.AddTransient<IAnswerService, AnswerService>();
+            services.AddTransient<IUserAnswerService, UserAnswerService>();
+
+        }
+    }
+}
