bc95f57d3cfac34724d1815a45c388003031490d
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-28 19:05:59 +0300

    Add files via upload

---
+++ C# Entity Framework Core/Exams/Exam 14 Aug 2020/DataProcessor/ImportDto/DepartmentCellInputModel.cs
@@ -0,0 +1,22 @@
+﻿namespace SoftJail.DataProcessor.ImportDto
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class DepartmentCellInputModel
+    {
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public IEnumerable<CellInputModel> Cells { get; set; }
+    }
+
+    public class CellInputModel
+    {
+        [Range(1,1000)]
+        public int CellNumber { get; set; }
+
+        public bool HasWindow{ get; set; }
+    }
+}
---
+++ C# Entity Framework Core/Exams/Exam 14 Aug 2020/DataProcessor/ImportDto/OfficerPrisonerInputModel.cs
@@ -0,0 +1,40 @@
+﻿using SoftJail.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Officer")]
+    public class OfficerPrisonerInputModel
+    {
+        [XmlElement("Name")]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [XmlElement("Money")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        public decimal Money { get; set; }
+
+        [XmlElement("Position")]
+        [EnumDataType(typeof(Position))]
+        public string Position { get; set; }
+
+        [XmlElement("Weapon")]
+        [EnumDataType(typeof(Weapon))]
+        public string Weapon { get; set; }
+
+        [XmlElement("DepartmentId")]
+        public int DepartmentId { get; set; }
+        
+        [XmlArray("Prisoners")]
+        public PrisonerInputModel[] Prisoners { get; set; }
+    }
+
+    [XmlType("Prisoner")]
+    public class PrisonerInputModel
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+
+    }
+}
