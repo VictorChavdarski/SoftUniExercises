0d5d5534362eb397d37f968806e18be51ef75500
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-03-28 15:33:05 +0300

    Add files via upload

---
+++ C# Entity Framework Core/Exams/Exam 14 Aug 2020/Data/Models/Officer.cs
@@ -0,0 +1,37 @@
+﻿namespace SoftJail.Data.Models
+{
+    using SoftJail.Data.Models.Enums;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Officer
+    {
+        public Officer()
+        {
+            this.OfficerPrisoners = new HashSet<OfficerPrisoner>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public Position Position { get; set; }
+
+        public Weapon Weapon { get; set; }
+
+        [ForeignKey(nameof(Department))]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+
+        public ICollection<OfficerPrisoner> OfficerPrisoners { get; set; }
+
+    }
+}
+
+
---
+++ C# Entity Framework Core/Exams/Exam 14 Aug 2020/Data/Models/Prisoner.cs
@@ -0,0 +1,46 @@
+﻿namespace SoftJail.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+
+    public class Prisoner
+    {
+        public Prisoner()
+        {
+            this.Mails = new HashSet<Mail>();
+            this.PrisonerOfficers = new HashSet<OfficerPrisoner>();
+        }
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Nickname { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime IncarcerationDate { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+
+        public decimal? Bail { get; set; }
+
+        [ForeignKey(nameof(Cell))]
+        public int? CellId { get; set; }
+
+        public Cell Cell { get; set; }
+
+        public ICollection<Mail> Mails { get; set; }
+
+        public ICollection<OfficerPrisoner> PrisonerOfficers { get; set; }
+
+    }
+}
+
