8d52e882045af4414253e860378a912c618177d6
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-04-05 23:56:38 +0300

    Add files via upload

---
+++ C# Entity Framework Core/Exams/Exam 04 Apr 2021/Data/Models/EmployeeTask.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.Data.Models
+{
+    public class EmployeeTask
+    {
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public Employee Employee { get; set; }
+
+        public int TaskId { get; set; }
+
+        [Required]
+        public Task Task { get; set; }
+    }
+}
---
+++ C# Entity Framework Core/Exams/Exam 04 Apr 2021/Data/Models/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.Data.Models
+{
+    public class Project
+    {
+        public Project()
+        {
+            this.Tasks = new HashSet<Task>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime OpenDate { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public ICollection<Task> Tasks { get; set; }
+
+    }
+}
