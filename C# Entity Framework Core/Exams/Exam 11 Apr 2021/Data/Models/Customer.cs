7e12137fafe7292cf801e1e7fb77efcaa250814e
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-04-16 23:10:30 +0300

    Add files via upload

---
+++ C# Entity Framework Core/Exams/Exam 11 Apr 2021/Data/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Data.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            this.Tickets = new HashSet<Ticket>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public decimal Balance  { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
