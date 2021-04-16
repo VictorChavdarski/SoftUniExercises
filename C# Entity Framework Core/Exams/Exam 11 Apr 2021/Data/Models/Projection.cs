5ebf229073a27f8d66636ca3c9062c261bc60742
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-04-16 23:11:21 +0300

    Add files via upload

---
+++ C# Entity Framework Core/Exams/Exam 11 Apr 2021/Data/Models/Projection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Data.Models
+{
+    public class Projection
+    {
+        public Projection()
+        {
+            this.Tickets = new HashSet<Ticket>();
+        }
+        public int Id { get; set; }
+
+        public int MovieId { get; set; }
+
+        public Movie Movie { get; set; }
+
+        [Required]
+        public DateTime DateTime { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; }
+
+    }
+}
