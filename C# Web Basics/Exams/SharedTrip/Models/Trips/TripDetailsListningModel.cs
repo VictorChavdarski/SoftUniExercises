c866485e3ff747b4b7538c090e001ee2801392b0
Victor Chavdarski <61271848+VictorChavdarski@users.noreply.github.com>
2021-06-27 23:57:39 +0300

    Add files via upload

---
+++ C# Web Basics/Exams/SharedTrip/Models/Trips/TripDetailsListningModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SharedTrip.Models.Trips
+{
+    public class TripDetailsListningModel
+    {
+        public string ImagePath { get; set; }
+
+        public string StartPoint { get; set; }
+
+        public string EndPoint { get; set; }
+
+        public string DepartureTime { get; set; }
+
+
+        public int Seats { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ C# Web Basics/Exams/SharedTrip/Models/Trips/TripListningViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SharedTrip.Models.Trips
+{
+    public class TripListningViewModel
+    {
+        public string Id { get; set; }
+
+        public string StartPoint { get; init; }
+
+        public string EndPoint { get; init; }
+
+        public string DepartureTime { get; init; }
+
+        public string ImagePath { get; init; }
+
+        public int Seats { get; init; }
+    }
+}
